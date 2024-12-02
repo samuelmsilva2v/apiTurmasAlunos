df1f28cf1bc4a18e57331a5c18c7a1c1ebe117e5
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-12-02 17:17:55 -0300

    model mapper

--- src/main/java/com/example/demo/domain/models/entities/Turma.java
+++ src/main/java/com/example/demo/domain/models/entities/Turma.java
@@ -4,15 +4,17 @@ import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "tb_turma")
+@Table(name = "tb_turmas")
 @Data
 public class Turma {
 
+	@Id
 	private UUID id;
 	private String numero;
 	private Integer anoLetivo;
