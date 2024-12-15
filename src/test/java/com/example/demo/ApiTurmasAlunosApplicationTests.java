0b8081bd0bba228ceff54cb1d795ee3e6f27809a
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-12-15 14:56:00 -0300

    cadastrar turma test

--- src/test/java/com/example/demo/ApiTurmasAlunosApplicationTests.java
+++ src/test/java/com/example/demo/ApiTurmasAlunosApplicationTests.java
@@ -1,13 +1,66 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.UUID;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.domain.models.dtos.TurmaRequestDto;
+import com.example.demo.domain.models.dtos.TurmaResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ApiTurmasAlunosApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private static UUID id;
+	
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void cadastrarTurmaTest() throws Exception {
+		
+		var faker = new Faker(Locale.forLanguageTag("pt-BR"));
+		
+		var request = new TurmaRequestDto();
+		request.setNumero(faker.number().digits(4));
+		request.setAnoLetivo(faker.number().randomDigit());
+		
+		var result = mockMvc.perform(post("/api/turmas")
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(request)))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		var content = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
+		
+		var response = objectMapper.readValue(content, TurmaResponseDto.class);
+		
+		assertNotNull(response.getId());
+		assertEquals(response.getNumero(), request.getNumero());
+		assertEquals(response.getAnoLetivo(), request.getAnoLetivo());
+		
+		id = response.getId();
 	}
 
 }
